e6879d5eb3c490e7fafb5b7418dff8730ea9beed
Lalith <lalithaditya.gaming@gmail.com>
2026-06-13 08:37:42 +0200

    act3

---
+++ Activity3.py
@@ -0,0 +1,13 @@
+x=5
+y="Lalith"
+print(x)
+print(y)
+
+Subject="Coding"
+print(Subject)
+
+name= input("Enter your name:")
+print("Your name is",name)
+
+favbook= input("What is your favourite book?")
+print(name,"your Favourite book is",favbook)
