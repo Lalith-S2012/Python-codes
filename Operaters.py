fa598148adb890e142f642b3da88aa3f6584cf4d
Lalith <lalithaditya.gaming@gmail.com>
2026-06-20 08:20:50 +0200

    operators

---
+++ Operaters.py
@@ -0,0 +1,12 @@
+Tree1=98
+Tree2=94
+Tree3=41
+Tree4=82
+Tree5=97
+
+Sum=Tree1+Tree2+Tree3+Tree4+Tree5
+print("The sum of all the trees is: ", Sum)
+
+average=Sum//5
+print("The average height of all the trees is: ", average)
+
