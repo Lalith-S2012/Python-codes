13095091056e6761101750bb038359a043c397db
Bhagath Seshabhatter <bhagaths@Perago.com>
2026-07-02 18:52:25 +0200

    New Folder

---
+++ AndOr.py
@@ -0,0 +1,25 @@
+a=19
+b=12
+c=0
+
+if a and b and c:
+    print("All the numbers have boolean values as true")
+else:
+    print("None of the numbers have a boolean value as false")
+    
+a=10
+b=-10
+c=0
+
+if a>0 or b>0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("none of the numbers are greater than 0")
+
+if b>0 or c>0:
+    print("either of the numbers is greater than 0")
+else:
+    print("None of the numbers are greater than 0")
+
+
+    
