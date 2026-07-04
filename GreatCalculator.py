a6afd3b0790dc847213775469ef961c19cc2e33d
Lalith-S2012 <lalithaditya.gaming@gmail.com>
2026-07-04 08:46:18 +0200

    GreatCalculator

---
+++ GreatCalculator.py
@@ -0,0 +1,8 @@
+marks = float(input("Enter your Marks. "))
+
+if marks >= 90:
+    print("You have got an A")
+elif marks <=89 and marks >=74:
+    print("You have got a B")
+else:
+    print("You have got a C, You can do better")
