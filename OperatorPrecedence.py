245a2583393569197413bc4f428de05474d087f5
Lalith-S2012 <lalithaditya.gaming@gmail.com>
2026-07-11 08:22:32 +0200

    Precendence

---
+++ OperatorPrecedence.py
@@ -0,0 +1,18 @@
+v = 4
+w = 5
+x= 6
+y = 13
+z = 9
+z = (v+w) *x / y;
+print("Value of (v+w) * x / y ", int(z))
+
+
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "John" and age >= 2 :
+    print("Hello! Welcome")
+else:
+    print("Goodbye")
+    
