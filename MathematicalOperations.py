6d9c26091d514b543b89d86924f7e7b992430e00
Lalith-S2012 <lalithaditya.gaming@gmail.com>
2026-09-03 18:41:51 +0200

    Mathematical Operations

---
+++ MathematicalOperations.py
@@ -0,0 +1,12 @@
+import math
+print("The Floor and Ceiling Value of 17.47 are: " + str(math.floor(17.47)) + ', '+ str(math.ceil(17.47)))
+
+x = 25
+y = -5
+
+print("The value of x after copying the sign from y is: " + str(math.copysign(x,y)))
+
+
+print("Absolute value of -23 and 147 are: " + str(math.fabs(-23)) + ", " + str(math.fabs(147)))
+
+print("The GCD of 36 and 60 : " + str(math.gcd(36, 60)))
